38fd301feb323f907d7e4f76d3e1886b6d25b177
Cody Cook <codycook33@gmail.com>
2018-09-12 15:44:02 -0700

    Added Movement!
    Click to move added as well as using the horizontal keys to turn the Camera around the player.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+
+	public LayerMask movementMask;
+	public PlayerMotor motor;
+	public Camera cam;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+		
+		
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, 100, movementMask))
+			{
+				//move the player to what we hit
+				motor.MoveToPoint(hit.point);
+
+			}
+		}
+	}
+}
