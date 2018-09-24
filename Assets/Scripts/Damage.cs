0a2432d335488b65c9fd6095cdb5007053749713
Cody Cook <codycook33@gmail.com>
2018-09-23 22:17:22 -0700

    Added partial scripts to attempt to move enemies to player
    the first will attempt to move the enemy to the player if within range, the second will do damage to the player if they are close enough!

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+
+	private bool attackPlayer;
+	// Use this for initialization
+	void Start () {
+		attackPlayer = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		
+			
+			if (col.gameObject.name == "Player")
+			{
+				attackPlayer = true;
+				Debug.Log ("I am hitting this thing!");
+			}
+	}
+
+	void OnTriggerExit (Collider col)
+	{
+		
+			
+			if (col.gameObject.name == "Player")
+			{
+				attackPlayer = false;
+				Debug.Log ("I cant hit this... yet.");
+			}
+	}
+}
---
+++ Assets/Scripts/PullMob.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullMob : MonoBehaviour {
+
+	public GameObject target;
+	
+	private bool attackPlayer;
+
+	// Use this for initialization
+	void Start () {
+		attackPlayer = false;
+	}
+	
+	void update () 
+	{
+		if (attackPlayer == true)
+		{
+			
+			transform.position = Vector3.MoveTowards(transform.position, target.transform.position, .03f);
+		}
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		
+			
+			if (col.gameObject.name == "Player")
+			{
+				attackPlayer = true;
+				Debug.Log ("I hit");
+			}
+	}
+
+	void OnTriggerExit (Collider col)
+	{
+		
+			
+			if (col.gameObject.name == "Player")
+			{
+				attackPlayer = false;
+				Debug.Log ("I am leaving it alone");
+			}
+	}
+}
+
